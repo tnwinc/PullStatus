fa4a3d87a0e29af6efcb8f02c66ab197806884f3
Chris Breiding <chrisbreiding@gmail.com>
2013-04-25 10:31:49 -0400

    update repositories styles

--- PullStatus/Settings/TNWSettingsViewController.m
+++ PullStatus/Settings/TNWSettingsViewController.m
@@ -12,6 +12,7 @@
 
 @interface TNWSettingsViewController ()
 @property BOOL loadingRepositories;
+- (void)setAppearance;
 @end
 
 @implementation TNWSettingsViewController
@@ -21,8 +22,9 @@
                                                       object:nil
                                                        queue:nil
                                                   usingBlock:^(NSNotification *note) {
-        [self loadRepositories];
-    }];
+                                                      [self loadRepositories];
+                                                  }];
+    [self setAppearance];
 }
 
 #pragma mark - Load Data
@@ -31,31 +33,31 @@
     if (self.loadingRepositories) return;
     self.loadingRepositories = YES;
     self.repositories = [[NSMutableArray alloc] init];
-
+    
     NSURL *url = [NSURL URLWithString:@"https://api.github.com/users/tnwinc/repos"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     AFJSONRequestOperation *operation;
     operation = [AFJSONRequestOperation
                  JSONRequestOperationWithRequest:request
-
-                                         success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-        NSMutableArray *repos = self.repositories;
-        //TODO: This loop should be out of the controller
-        for (NSDictionary *item in JSON) {
-            [repos addObject:[Repository instanceFromDictionary:item]];
-        }
-
-        self.loadingRepositories = NO;
-        [self.activityView stopAnimating];
-        [self.repositoriesTableView reloadData];
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"RepositoriesLoaded" object:self];
-    }
-
-                                         failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-        NSLog(@"Error: loading repositories: %@", error);
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"RepositoriesFailedToLoad" object:error];
-    }];
-
+                 
+                 success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+                     NSMutableArray *repos = self.repositories;
+                     //TODO: This loop should be out of the controller
+                     for (NSDictionary *item in JSON) {
+                         [repos addObject:[Repository instanceFromDictionary:item]];
+                     }
+                     
+                     self.loadingRepositories = NO;
+                     [self.activityView stopAnimating];
+                     [self.repositoriesTableView reloadData];
+                     [[NSNotificationCenter defaultCenter] postNotificationName:@"RepositoriesLoaded" object:self];
+                 }
+                 
+                 failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
+                     NSLog(@"Error: loading repositories: %@", error);
+                     [[NSNotificationCenter defaultCenter] postNotificationName:@"RepositoriesFailedToLoad" object:error];
+                 }];
+    
     [self.activityView startAnimating];
     [operation start];
 }
@@ -69,19 +71,31 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *cellIdentifier = @"RCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-
+    
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellIdentifier];
+        
+        UIView *cellBackgroundView = [[UIView alloc] initWithFrame:cell.frame];
+        cellBackgroundView.backgroundColor = [UIColor blackColor];
+        cell.selectedBackgroundView = cellBackgroundView;
+        cell.textLabel.textColor = [UIColor whiteColor];
     }
-
+    
     Repository *repo = [self.repositories objectAtIndex:indexPath.item];
     assert(repo);
-
+    
     cell.textLabel.text = repo.name;
     cell.detailTextLabel.text = repo.descriptionText;
-
+    
     assert(cell);
     return cell;
+}
+
+#pragma mark - Appearance
+
+- (void)setAppearance {
+    UIImage *barButton = [[UIImage imageNamed:@"barButton.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 14, 0, 5)];
+    [self.authenticationButton setBackgroundImage:barButton forState:UIControlStateNormal];
 }
 
 @end
