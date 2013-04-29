9c614f1e8ee2b2b95c7cfd8cfddfe0d1a771ecd3
Brendan Erwin <brendanjerwin@gmail.com>
2013-04-29 14:44:56 -0400

    Now looks for preexisting authorizations
    Has a bit of a timing error though. The list of authorizations has
    cache headers and also might be eventually consistent.

--- PullStatus/Settings/TNWGitHubOathTokenRetriever.m
+++ PullStatus/Settings/TNWGitHubOathTokenRetriever.m
@@ -14,22 +14,46 @@
                         andClient:(TNWHttpClient *)client
                           success:(void (^)(NSString *))success
                           failure:(void (^)(NSError *))failure {
-    
+
     NSDictionary *params = @{ @"client_id": kGitHubApiClientID, @"client_secret": kGitHubApiClientSecret, @"scopes": @[@"repo"] };
-    
+
     client.parameterEncoding = AFJSONParameterEncoding;
     [client setAuthorizationHeaderWithUsername:aUser password:password];
-    [client postPath:@"/authorizations" parameters:params
-      success:^(AFHTTPRequestOperation *operation, id responseObject) {
 
-          NSMutableDictionary *response = (NSMutableDictionary *) responseObject;
-          NSString *token = response[@"token"];
-          [client setAuthorizationHeaderWithBearerToken:token];
-          if (success) success(token);
-          
+    void (^ setToken)(NSDictionary *) = ^(NSDictionary *item) {
+        NSString *token = item[@"token"];
+        [client setAuthorizationHeaderWithBearerToken:token];
+        if (success) success(token);
+    };
+
+    void (^ getNewToken)(void) = ^{
+        [client postPath:@"authorizations" parameters:params
+                 success:^(AFHTTPRequestOperation *operation, id responseObject) {
+            NSMutableDictionary *response = (NSMutableDictionary *)responseObject;
+            NSLog(@"Got new authorization");
+            setToken(response);
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"Error while retrieving OAuth Token: %@", error);
+            if (failure) failure(error);
+        }];
+    };
+
+    [client getPath:@"authorizations" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSPredicate *findOurApp = [NSPredicate predicateWithFormat:@"app.url == %@", @"https://github.com/tnwinc/PullStatus"];
+        NSArray *authorizations = responseObject;
+        authorizations = [authorizations filteredArrayUsingPredicate:findOurApp];
+
+        if (authorizations.count > 0) {
+            NSDictionary *entry = [authorizations lastObject];
+            NSLog(@"Found existing authorization.");
+            setToken(entry);
+        } else {
+            NSLog(@"No existing authorizations, getting a new one");
+            getNewToken();
+        }
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        NSLog(@"Error while retrieving OAuth Token: %@", error);
-        if (failure) failure(error);
+        NSLog(@"Error while checking for existing token: %@", error);
+        getNewToken();
     }];
 }
 
